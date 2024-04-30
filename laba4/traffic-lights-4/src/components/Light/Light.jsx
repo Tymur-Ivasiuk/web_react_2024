6ec2326d19ed5301d2511c297a9bc6489297efe5
Tymur Ivasiuk <ivasiuktymur@gmail.com>
2024-05-01 00:03:39 +0300

    laba4 done

--- laba4/traffic-lights-4/src/components/Light/Light.jsx
+++ laba4/traffic-lights-4/src/components/Light/Light.jsx
@@ -2,21 +2,24 @@ import React, { useState, useEffect } from 'react';
 import s from './Light.module.css';
 
 const Light = (props) => {
-    const [count, setCount] = useState(0);
-
     return (
         <div 
             className={s.light} 
-            style={{background: props.tlColor}} 
-            onClick={() => setCount(count + 1)}
+            style={{background: props.lightItem.color}} 
+            onClick={() => props.clickLight(props.lightItem.id)}
         >
-            {count}
+            {props.lightItem.clickcount}
         </div>
     );
 }
 
 Light.defaultProps = {
-    tlColor: "red",
+    lightItem: {
+        id: 1,
+        color: 'red',
+        description: "Червоний",
+        clickcount: 0
+    }
 };
 
 export default Light;
